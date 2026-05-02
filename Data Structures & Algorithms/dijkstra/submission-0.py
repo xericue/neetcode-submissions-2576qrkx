f00e51fa77efe7b978d619148a3fab3889cd3697
xericue <106630482+xericue@users.noreply.github.com>
2026-05-02 10:27:39 -0400

    Add: dijkstra - submission-0

---
+++ Data Structures & Algorithms/dijkstra/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def shortestPath(self, n: int, edges: List[List[int]], src: int) -> Dict[int, int]:
+        # first, create an adjacency list if not already given
+        adj = {} # adj lists are actually dictionaries of lists Lollll
+        for i in range(n):
+            adj[i] = []
+
+        for source, dest, weight in edges:
+            # for each source, link its neighbors - get every weight 
+            # in the adjacency list alongside the destination
+            adj[source].append([dest, weight])
+        
+        # okay now we have our adjacency list/we can continue
+        
+        distances = {}
+        for i in range(n):
+            distances[i] = float('inf')
+
+        predecessors = {}
+        minHeap = [(0, src)]
+        distances[src] = 0
+
+        while minHeap:
+            # take the shortest path every time type stuff
+            curr_dist, curr_vtx = heapq.heappop(minHeap)
+            if curr_dist > distances[curr_vtx]:
+                continue
+            
+            for dest, weight in adj[curr_vtx]:
+                new_dist = curr_dist + weight
+                if new_dist < distances[dest]:
+                    distances[dest] = new_dist
+                    predecessors[dest] = curr_vtx
+                    heapq.heappush(minHeap, (new_dist, dest))
+
+        for i in range(n):
+            if distances[i] == float('inf'):
+                distances[i] = -1
+        return distances
+                
