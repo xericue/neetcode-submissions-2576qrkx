1e9186a559fea74b80d53f102c151caf1c5f6f97
xericue <106630482+xericue@users.noreply.github.com>
2026-05-18 08:08:42 -0400

    Add: combination-target-sum - submission-15

---
+++ Data Structures & Algorithms/combination-target-sum/submission-15.py
@@ -0,0 +1,25 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        curr_sum = []
+        ret_arr = []
+
+        def helper(i, nums, curr_sum, ret_arr):
+            # base case - if sum(curr_sum) == target
+            if sum(curr_sum) == target:
+                ret_arr.append(curr_sum.copy())
+                return
+            
+            # second base case if its impossible to find the combination
+            if sum(curr_sum) > target:
+                return
+
+            if i >= len(nums):
+                return
+
+            curr_sum.append(nums[i])
+            helper(i, nums, curr_sum, ret_arr)
+            curr_sum.pop()
+            helper(i + 1, nums, curr_sum, ret_arr)
+
+        helper(0, nums, curr_sum, ret_arr)
+        return ret_arr
