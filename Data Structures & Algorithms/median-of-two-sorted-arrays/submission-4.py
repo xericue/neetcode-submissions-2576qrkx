eb5717fe6ad1917d0dbe1bc1b494d4b96be2ee6b
xericue <106630482+xericue@users.noreply.github.com>
2026-06-19 21:33:46 -0400

    Add: median-of-two-sorted-arrays - submission-4

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-4.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
+        total = len(A) + len(B)
+        half = total // 2 # integer divison
+
+        # run BS on the smaller of the two arrays
+        if len(B) < len(A):
+            A, B = B, A
+
+        left, right = 0, len(A) - 1
+        while True: # lazy???
+            mid_a = (left + right) // 2 # A
+            mid_b = half - mid_a - 1 - 1 # INDEX # arrays
+            # are indexed at zero... so get the numebr of
+            # values
+
+            # if mid_a is still in bounds, use it; otherwise, default it to -inf
+            # negative infinity as the left partition to compare in case our
+            # middle is too bad
+            A_left_part = A[mid_a] if mid_a >= 0 else -(float("inf"))
+            A_right_part = A[mid_a + 1] if mid_a + 1 < len(A) else float("inf")
+            
+            B_left_part = B[mid_b] if mid_b >= 0 else -(float("inf"))
+            B_right_part = B[mid_b + 1] if mid_b + 1 < len(B) else float("inf")
+            
+            # gone too far to right - compare to infinity
+            if mid_b + 1 < len(B):
+                B_right_part = B[mid_b + 1] # value RIGHT after
+            else:
+                B_right_part = float("inf")
+
+            if A_left_part <= B_right_part and B_left_part <= A_right_part:
+                # odd
+                if total % 2:
+                    return min(A_right_part, B_right_part)
+                # even
+                return (max(A_left_part, B_left_part) + min(A_right_part, B_right_part)) / 2
+            
+            elif A_left_part > B_right_part:
+                right = mid_a - 1
+            else:
+                left = mid_a + 1
