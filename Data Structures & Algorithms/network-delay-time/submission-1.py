b3b8df18b38d50ee2835014c2be96fd0403081a7
xericue <106630482+xericue@users.noreply.github.com>
2026-07-03 10:16:25 -0400

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # so this is dijkstra's but why do we need to maintain
+        # and use a visited set
+
+        dist = [float('inf')] * (n + 1)
+        visited = set()
+        # create adj list
+        adj = {i : [] for i in range(1, n + 1)}
+        # path not needed
+
+        for ui, vi, ti in times:
+            adj[ui].append((ti, vi))
+
+        q = [(0, k)]
+
+        t = 0 # this is to store the latest time because the latest time
+        # will indicate how long it took to get the absolute longest path connected
+        # in the entire graph
+        while q:
+            weight, node = heapq.heappop(q)
+
+            if node in visited:
+                continue
+            visited.add(node)
+            t = weight
+            # processing adjacent vertices
+            for n_wei, nei in adj[node]:
+                if weight + n_wei < dist[nei]:
+                    # recalculate distance
+                    dist[nei] = weight + n_wei
+                    heapq.heappush(q, (weight + n_wei, nei))
+
+        if len(visited) == n:
+            return t
+        return -1
