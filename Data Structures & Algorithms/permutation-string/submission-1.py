f447aab78cec6d5c9d51fd5764f8b4c97ebaed04
xericue <106630482+xericue@users.noreply.github.com>
2026-08-07 13:03:04 -0400

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1 = len(s1)
+        n2 = len(s2)
+
+        if n1 > n2:
+            return False
+        s1_counts = [0] * 26
+        s2_counts = [0] * 26
+
+        # same logic for sliding window but now with the weird logic of ord() and stuff?
+
+        # build the counts
+        for i in range(n1):
+            s1_counts[ord(s1[i]) - 97] += 1
+            s2_counts[ord(s2[i]) - 97] += 1
+        
+        if s1_counts == s2_counts: # this is actually O(1) because its guaranteed to be O(26)
+            return True
+        
+        for j in range(n1, n2):
+            s2_counts[ord(s2[j]) - 97] += 1
+            s2_counts[ord(s2[j - n1]) - ord('a')] -= 1
+
+            if s1_counts == s2_counts:
+                return True
+        
+        return False
